ab04d6e641a7f614145d9c3c19bc4858196bc34f
Ren Aysha <aysha@viki.com>
2014-08-16 17:22:44 +0800

    Move mainbar code in application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :show_title
+
+  def show_title
+  	@show_title = Cinema.find_by_id(params[:id]).show.name unless Cinema.find_by_id(params[:id]).nil?
+  end
 end
